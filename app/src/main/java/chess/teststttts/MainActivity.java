8316d7b085248e5166abc1c3e6d3bb293bd12fe5
chester <just13.0818@gmail.com>
2019-03-19 14:42:32 +0800

    1. Add permission for STT (for using microphone) 2. Implement STT 3. Add STT status on UI 4. Adjust some UI size or color 5. Assign STT language strings in java code for    given example what data should use

--- app/src/main/java/chess/teststttts/MainActivity.java
+++ app/src/main/java/chess/teststttts/MainActivity.java
@@ -2,6 +2,9 @@ package chess.teststttts;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -10,14 +13,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "testSTTTTS";
     private boolean DBG = true;
 
-    private Spinner mSpinnerSTTlang;
+    private TextView mSTT_status;
+    private String mSTT_lang = null;
+    private EditText mSTT_result;
+    private STT_Listener mSTT_listener = new STT_Listener();
+
+    // STT service
+    private SpeechRecognizer mSR = null;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -55,19 +70,45 @@ public class MainActivity extends AppCompatActivity {
         initViews();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Make sure to release STT.
+        if (null != mSR) {
+            mSR.destroy();
+            mSR = null;
+        }
+    }
+
     // To setup views
     private void initViews() {
         // The Navigation Buttons
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        // STT status sequence:
+        //   uninitiated -> initialed -> listening
+        //   -> stopped -> listening -> stopped -> listening -> stopped -> ...
+        mSTT_status = findViewById(R.id.stt_status);
+        mSTT_status.setText(R.string.uninitiated);
+
         // STT language spinner
-        mSpinnerSTTlang = findViewById(R.id.spinner_stt);
-        ArrayAdapter<CharSequence> sttLangList = ArrayAdapter.createFromResource(MainActivity.this,
-                R.array.stt_language,
-                android.R.layout.simple_spinner_dropdown_item);
-        mSpinnerSTTlang.setAdapter(sttLangList);
-        mSpinnerSTTlang.setOnItemSelectedListener(mSpinnerSTTlang_Listener);
+        Spinner spinnerSTTlang = findViewById(R.id.spinner_stt);
+        String[] stt_langs = {
+                getString(R.string.system_default),
+                Locale.TAIWAN.toString(),
+                Locale.US.toString(),
+                Locale.JAPAN.toString(),
+                Locale.GERMANY.toString()
+        };
+        ArrayAdapter<String> sttLangList = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, stt_langs);
+        spinnerSTTlang.setAdapter(sttLangList);
+        spinnerSTTlang.setOnItemSelectedListener(mSpinnerSTTlang_Listener);
+
+        // Will update STT result on this EditText view
+        mSTT_result = findViewById(R.id.stt_result);
     }
 
     // All buttons onClick listener
@@ -75,30 +116,113 @@ public class MainActivity extends AppCompatActivity {
         switch (v.getId()) {
             case R.id.button_stt_init:
                 if (DBG) Log.d(TAG, "onClick, stt init");
+                if (null == mSR) {
+                    mSR = SpeechRecognizer.createSpeechRecognizer(this);
+                    mSTT_status.setText(R.string.initialed);
+
+                    // We disable init button because only need one initialization,
+                    // and then enable Start & Stop buttons.
+                    v.setEnabled(false);
+                    findViewById(R.id.button_stt_start).setEnabled(true);
+                    findViewById(R.id.button_stt_stop).setEnabled(true);
+                }
                 break;
 
             case R.id.button_stt_start:
-                if (DBG) Log.d(TAG, "onClick, stt start");
+                if (DBG) Log.d(TAG, "onClick, stt start, mSTT_lang="+mSTT_lang);
+                if (null != mSR) {
+                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    // Change STT language if user selected.
+                    if (null != mSTT_lang) {
+                        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, mSTT_lang);
+                    }
+                    mSR.setRecognitionListener(mSTT_listener);
+                    mSR.startListening(intent);
+                    mSTT_status.setText(R.string.listening);
+                }
                 break;
 
             case R.id.button_stt_stop:
                 if (DBG) Log.d(TAG, "onClick, stt stop");
+                if (null != mSR) {
+                    mSR.destroy();
+                    mSTT_status.setText(R.string.stopped);
+                }
                 break;
 
             default:
                 break;
         }
-        Log.d(TAG, "onClick, mSpinnerSTTlang.getPrompt()="+mSpinnerSTTlang.getAutofillValue());
     }
 
     AdapterView.OnItemSelectedListener mSpinnerSTTlang_Listener = new AdapterView.OnItemSelectedListener() {
         public void onItemSelected(AdapterView<?> parent,
                                    View view, int pos, long id) {
-            if (DBG) Log.d(TAG, "onSpinnerSTTlang selected: "+parent.getItemAtPosition(pos).toString());
+            final String selected_str = parent.getItemAtPosition(pos).toString();
+            if (DBG) Log.d(TAG, "onSpinnerSTTlang selected: " + selected_str);
+
+            // Update mSTT_lang.
+            if (getString(R.string.system_default).equals(selected_str)) {
+                mSTT_lang = null;
+            } else {
+                mSTT_lang = selected_str;
+            }
         }
 
         public void onNothingSelected(AdapterView parent) {
             // Do nothing.
         }
     };
+
+
+    class STT_Listener implements RecognitionListener
+    {
+        public void onReadyForSpeech(Bundle params) {
+            if (DBG) Log.d(TAG, "onReadyForSpeech");
+        }
+
+        public void onBeginningOfSpeech() {
+            if (DBG) Log.d(TAG, "onBeginningOfSpeech");
+        }
+
+        public void onRmsChanged(float rmsdB) {
+            if (DBG) Log.d(TAG, "onRmsChanged");
+        }
+
+        public void onBufferReceived(byte[] buffer) {
+            if (DBG) Log.d(TAG, "onBufferReceived");
+        }
+
+        public void onEndOfSpeech() {
+            if (DBG) Log.d(TAG, "onEndofSpeech");
+            mSTT_status.setText(R.string.stopped);
+        }
+
+        public void onError(int error) {
+            Log.e(TAG, "onError: " + error);
+            mSTT_result.setText(error);
+        }
+
+        public void onResults(Bundle results)
+        {
+            try {
+                // Get STT result and update it to UI.
+                ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if (null != data) {
+                    String str = data.get(0).toString();
+                    mSTT_result.setText(str);
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "got exception, e="+e);
+            }
+        }
+
+        public void onPartialResults(Bundle partialResults) {
+            Log.d(TAG, "onPartialResults");
+        }
+
+        public void onEvent(int eventType, Bundle params) {
+            Log.d(TAG, "onEvent " + eventType);
+        }
+    }
 }
